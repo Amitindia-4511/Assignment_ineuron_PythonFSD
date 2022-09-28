feee15d6def35f2b169e01bdafcf609401debcf4
Amit Kumar <amitindia4511@gmail.com>
2022-09-28 22:18:52 +0530

    Assignment1 for Python Full Stack

---
+++ assignment1.py
@@ -0,0 +1,48 @@
+#1 Write a command to get the Python version you are using.
+
+import sys
+print(sys.version)
+
+#2 Write a python script to print Hello Python on the screen directly in the command line.
+    ## Done
+#3 Write a python script and store it in a file and execute the file from the command line.
+  ## Done
+
+#4 Write a python script to print Hello Python on the screen using visual studio code.
+  ## Done
+
+#5 Write a python script to print Hello on the first line and Python on the second line
+print('Hello')
+print('Python')
+
+#6 Write a python script to print “MySirG” on the screen
+print('MySirG')
+
+#7. Write a python script to print “Teacher’s Day” on the screen
+print('“Teacher’s Day”')
+
+# 8. Write a python script to print the value of a variable. Variable contains your name as
+# data.
+
+name = 'Amit'
+print(name)
+
+# 9. Write a python script to create variables to store your name, age, qualification, years
+# of experience and print it.
+
+name='Amit'
+age=24
+qualification='B.Tech'
+experiance = 'Intermediate'
+
+print('Name=',name,'Age=',age,'Qualification=',qualification,'Experiance=',experiance)
+
+# 10. Write a python script to print values of three variables, each in a new line. Variable
+# contains integer values
+
+a=12
+b=23
+c=56
+print(a)
+print(b)
+print(c)
